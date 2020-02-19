2731e547993948b12c5e886ad244dacdff214f30
LinZhuming <9208608@qq.com>
2020-02-19 23:59:25 +0800

    update dishlist add dish-detail

---
+++ src/components/DishDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>{{data}}</h1>
+    <img :src="'http://127.0.0.1:8090/dish/' + data[0].imgUrl" alt="">
+  </div>
+</template>
+<script>
+export default {
+  props: ['did'],
+  data() {
+    return {
+      data: [],
+    }
+  },
+  created() {
+    let url = `${this.$store.state.globalSettings.apiUrl}/admin/dish/${this.did}`;
+    this.$axios
+    .get(url)
+    .then(({data}) => {
+      this.data = data;
+    }).catch(err => console.warn(err))
+  }
+}
+</script>
--- src/views/DishList.vue
+++ src/views/DishList.vue
@@ -2,19 +2,64 @@
   <div class="cat-dish-list">
     <zoom-breadcrumb :op="breadcrumbOp"></zoom-breadcrumb>
     <br>
-    <zoom-button type="primary" size="large">添加新的菜品类别</zoom-button>
+    <zoom-button @click="toAdd" type="primary" size="large">添加新的菜品</zoom-button>
     <hr>
-    <zoom-grid ref="grid" :op="gridOp"></zoom-grid>
+    <zoom-tabs :value="curTab" @tabChange="tabChange">
+      <zoom-tab-item :index="0" label="所有菜品">
+        <zoom-row>
+          <zoom-col span="4">
+            菜品类别　<zoom-dropdown v-model="category" :op="dropdownOp"></zoom-dropdown>
+          </zoom-col>
+          <zoom-col span="4">
+            菜品名称　<zoom-input v-model.trim="dishName"></zoom-input>
+          </zoom-col>
+          <zoom-col span="2">
+            <zoom-button @click="load">搜索</zoom-button>
+          </zoom-col>
+        </zoom-row>
+        <zoom-grid ref="grid" :op="gridOp"></zoom-grid>
+        <zoom-pager ref="page" :op="pageOp"></zoom-pager>
+      </zoom-tab-item>
+      <zoom-tab-item v-show="showDetail" :index="1">
+        <h5 slot="label">
+          菜品详情
+          <i @click.stop="close" class="zoom-icon icon-close-plus"></i>
+        </h5>
+        <dish-detail :did="did"></dish-detail>
+      </zoom-tab-item>
+    </zoom-tabs>
   </div>
 </template>
 <script>
-import Table from "../components/Table";
+import dishDetail from "../components/DishDetail";
 export default {
   components: {
-    catTable: Table
+    dishDetail
   },
   data() {
     return {
+      did: '',  //  菜品id
+      showDetail: false,  //  展示详情页
+      category: '', //  菜品类别
+      dishName: '', //  菜品名称
+      curTab: 0, // 当前激活的tab索引
+      pageVal: {
+        total: 0,	// 总条数
+        curPage: 1,	// 展示的当前页
+        pageSize: 10	// 每页要展示多少条数据
+      },
+      dropdownOp: {
+        placeHolder: '--请选择--',	//占位符
+        // defalut: '5',			//默认选中的值, 写value
+        readonly: true,		//是否禁止输入
+        data: [						//下拉框数据, 键值对的方式, text是展示的文本
+          {value: 1, text: '肉类'},
+          {value: 2, text: '海鲜河鲜'},
+          {value: 3, text: '丸滑类'},
+          {value: 4, text: '蔬菜豆制品'},
+          {value: 5, text: '菌菇类'}
+        ],
+      },
       breadcrumbOp: {
         data: [
           {id: 1, url: '#/main', title: '首页'},
@@ -25,7 +70,7 @@ export default {
       gridOp: {
         title: [
           {
-            fieId: 'did',
+            fieId: '',
             header: '编号'
           },
           {
@@ -33,12 +78,15 @@ export default {
             header: '操作',
             btns: [
               {
-                title: '修改',
+                title: '详情',
                 css: {
                   icon: 'icon-edit'
                 },
                 onClick: val => {
-                  // this.updateCategory(val);
+                  console.log(val);
+                  this.did = val.did;
+                  this.curTab = 1;
+                  this.showDetail = true;
                 }
               },
               {
@@ -52,31 +100,70 @@ export default {
               }
             ]
           },
-           {
+          {
+            fieId: 'did',
+            header: '菜品编号'
+          },
+          {
             fieId: 'title',
             header: '菜品名称'
           },
           {
-            fieId: 'price',
-            header: '价格(¥)'
-          },
-          {
             fieId: 'categoryId',
             header: '菜品类别'
+          },
+          {
+            fieId: 'price',
+            header: '价格(¥)'
           }
         ],
         data: []
+      },
+      pageOp: {
+        pageSizes: [5, 10, 20],	// 可选择每页展示数量
+        pageSizeSkip: (val, pageVal) => {
+          this.pageVal.pageSize = val;	// 每页大小的下拉框数据发生改变事件
+          this.load();
+        },
+        beforeSkip: (val, pageVal) => {
+          console.log('要跳转到',val);	//跳转前事件
+        },
+        skip: (val, pageVal) => {
+          this.pageVal.curPage = val;
+          this.pageVal.pageSize = pageVal.pageSize;
+          this.load();
+          console.log('当前页是',val, pageVal);	//点击跳转触发获取当前页
+        },
       }
     };
   },
   mounted() {
-    this.load();
+    this.$nextTick(() => {
+      this.load();
+    })
   },
   methods: {
+    toAdd() {
+      this.$router.push('/dish/add');
+    },
+    /**
+     * 关闭详情页, 加载菜品
+     */
+    close() {
+      this.curTab = 0;
+      this.showDetail = false;
+      this.tabChange(0);
+    },
+    tabChange(index) {
+      this.curTab = index;
+      this.$nextTick(() => {
+        this.load();
+      })
+    },
     load() {
       this.$refs['grid'].showLoad(true);
       // 加载菜品列表
-      let url = this.$store.state.globalSettings.apiUrl + "/admin/dish?curPage=1&pageSize=10&categoryId=";
+      let url = `${this.$store.state.globalSettings.apiUrl}/admin/dish?curPage=${this.pageVal.curPage}&pageSize=${this.pageVal.pageSize}&categoryId=${this.category}&title=${this.dishName}`
       this.$axios
       .get(url)
       .then(({ data }) => {
@@ -102,7 +189,13 @@ export default {
                 break;
             }
           });
+          this.pageVal = {
+            total: data.data.total,	// 总条数
+            curPage: data.data.curPage,	// 展示的当前页
+            pageSize: data.data.pageSize	// 每页要展示多少条数据
+          }
           this.$refs['grid'].load(arr);
+          this.$refs['page'].load(this.pageVal);
         }
         this.$refs['grid'].showLoad(false);
       })
@@ -116,6 +209,9 @@ export default {
 <style lang="scss">
 .cat-dish-list {
   td[fieid="did"] {
+    display: none;
+  }
+  th[col="3"] {
     display: none;
   }
 }
